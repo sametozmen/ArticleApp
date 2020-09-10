aac767e7ead1a434b1c2b9f2fa03a35c3d401e51
R.Samet Özmen <sametozmen24@gmail.com>
2020-09-10 13:21:53 +0300

    CreateWriterEntity

---
+++ ArticleApp.Entities/Writer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ArticleApp.Entities
+{
+    public class Writer:BaseEntity
+    {
+        [Required]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter a valid username.")]
+        [StringLength(150)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string LastName { get; set; }
+    }
+}
