fd82e6769853e418331b23caadd627182d2394fc
R.Samet Özmen <sametozmen24@gmail.com>
2020-09-10 20:22:30 +0300

    seed db and migrations

---
+++ ArticleApp.DataAccess/Concrete/SeedDB.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ArticleApp.Entities;
+using System.Linq;
+
+namespace ArticleApp.DataAccess.Concrete
+{
+    public static class SeedDB
+    {
+        public static void Seed()
+        {
+            var context = new ArticleContext();
+
+            if (context.Database.GetPendingMigrations().Count() == 0)
+            {
+                if (context.Categories.Count() == 0)
+                {
+                    context.Categories.AddRange(Categories);
+                }
+
+                if (context.Articles.Count() == 0)
+                {
+                    context.Articles.AddRange(Articles);
+                    
+                }
+                if (context.Writers.Count()==0)
+                {
+                    context.Writers.AddRange(Writers);
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        private static Category[] Categories = {
+            new Category() { CategoryName="Telefon"},
+            new Category() { CategoryName="Bilgisayar"},
+            new Category() { CategoryName="Elektronik"}
+        };
+
+        private static Writer[] Writers = {
+            new Writer() { Name="Mehmet"},
+            new Writer() { Name="Ahmet"},
+            new Writer() { Name="Ali"}
+        };
+        private static Article[] Articles =
+        {
+            new Article(){ ArticleHeader="Samsung S5", CoverLetter="test cover letter 1 ", Text="test text"},
+            new Article(){ ArticleHeader="Samsung S5", CoverLetter="test cover letter 1 ", Text="test text"}
+        };
+
+
+    }
+}
---
+++ ArticleApp.DataAccess/Migrations/20200910170851_createdb.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ArticleApp.DataAccess.Migrations
+{
+    public partial class createdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: true),
+                    CreatedUser = table.Column<int>(nullable: true),
+                    ModifiedUser = table.Column<int>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    ArticleHeader = table.Column<string>(nullable: true),
+                    CoverLetter = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true),
+                    CategoryID = table.Column<int>(nullable: false),
+                    WriterID = table.Column<int>(nullable: false),
+                    ViewCounter = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categorie",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: true),
+                    CreatedUser = table.Column<int>(nullable: true),
+                    ModifiedUser = table.Column<int>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Writers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: true),
+                    CreatedUser = table.Column<int>(nullable: true),
+                    ModifiedUser = table.Column<int>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    Username = table.Column<string>(maxLength: 150, nullable: false),
+                    Name = table.Column<string>(maxLength: 150, nullable: false),
+                    LastName = table.Column<string>(maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Writers", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Writers");
+        }
+    }
+}
