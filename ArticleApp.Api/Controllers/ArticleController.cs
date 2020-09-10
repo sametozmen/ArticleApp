0f2c48a2ee86c17867e8776ad46e469d95529336
R.Samet Özmen <sametozmen24@gmail.com>
2020-09-10 19:55:48 +0300

    Controller oluşturuldu.

---
+++ ArticleApp.Api/Controllers/ArticleController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ArticleApp.Business.Abstract;
+using ArticleApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArticleApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly IArticleServices _service;
+        public int GetWriterID()
+        {
+            return int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+        }
+        public ArticleController(IArticleServices service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public IActionResult Get()
+        {
+            var list = _service.GetAll();
+
+            if (list.Count() > 0)
+            {
+                return Ok(list);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+        [HttpGet]
+        [Route("CategoryById")]
+        public IActionResult GetCategoriById(int id)
+        {
+            var articles = _service.GetAll().Where(a => a.CategoryID == id);
+
+            if (articles != null)
+            {
+                return Ok(articles);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+        [HttpGet]
+        [Route("Search")]
+        public IActionResult Search(string text)
+        {
+            var search = _service.Search(text);
+
+            if (search.Count() > 0)
+            {
+                return Ok(search);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [Route("AddEntity")]
+        public IActionResult Add([FromBody] Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                article.CreatedDate = DateTime.Now;
+                article.WriterID = GetWriterID();
+                article.IsActive = true;
+                article.IsDeleted = false;
+
+                _service.Add(article);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public IActionResult Update(int articleID, [FromBody] Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                try
+                {
+                    var model = _service.GetById(articleID);
+                    if (model == null)
+                    {
+                        return NotFound();
+                    }
+
+                    else
+                    {
+                        if (model.WriterID != GetWriterID())
+                        {
+                            return BadRequest("Bu Makale Başka Bir yazarımıza aittir.");
+                        }
+                        article.WriterID = GetWriterID();
+                        article.UpdatedDate = DateTime.Now;
+                        article.ModifiedUser = GetWriterID();
+                        _service.Update(article);
+                        return Ok();
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    return BadRequest(ex.Message);
+                }
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public IActionResult Delete(Article entity)
+        {
+            try
+            {
+
+                _service.Delete(entity);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        
+    }
+}
---
+++ ArticleApp.Rest/Controllers/ArticleController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArticleApp.Rest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly Iarticleservice _service;
+
+        public ArticleController(IMakaleService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public IActionResult Get()
+        {
+            var list = _service.GetAll();
+
+            if (list.Count() > 0)
+            {
+                return Ok(list);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+        [HttpGet]
+        [Route("makale")]
+        public IActionResult Get(int id)
+        {
+            var makale = _service.GetById(id);
+
+            if (makale != null)
+            {
+                return Ok(makale);
+            }
+            else
+            {
+                return NotFound("Could not find the makale");
+            }
+        }
+
+
+        [HttpGet]
+        [Route("kategoriMakale")]
+        public IActionResult GetKategoriById(int id)
+        {
+            var makale = _service.GetAll().Where(_ => _.KategoriId == id);
+
+            if (makale != null)
+            {
+                return Ok(makale);
+            }
+            else
+            {
+                return NotFound("Could not find the makale");
+            }
+        }
+
+
+        [HttpGet]
+        [Route("search")]
+        public IActionResult GetSearchAll(string search)
+        {
+            var makaleSearch = _service.GetSearchAll(search);
+
+            if (makaleSearch.Count() > 0)
+            {
+                return Ok(makaleSearch);
+            }
+            else
+            {
+                return NotFound("Could not find the makale");
+            }
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add([FromBody] Makale makale)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                makale.EklenmeTarihi = DateTime.Now;
+                makale.YazarId = GetYazarId();
+                _service.Add(makale);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IActionResult Update(int makaleId, [FromBody] Makale makale)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                try
+                {
+                    var model = _service.GetById(makaleId);
+                    if (model == null)
+                    {
+                        return NotFound();
+                    }
+
+                    else
+                    {
+                        if (model.YazarId != GetYazarId())
+                        {
+                            return BadRequest("Bu Makale Başka Bir yazarımıza aittir.");
+                        }
+                        makale.YazarId = GetYazarId();
+                        makale.GuncellenmeTarihi = DateTime.Now;
+                        _service.Update(makale);
+                        return Ok();
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    return BadRequest(ex.Message);
+                }
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public IActionResult Delete(int makaleId)
+        {
+            try
+            {
+                if (_service.GetById(makaleId) != null)
+                {
+                    _service.Delete(makaleId);
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound("Makale Bulunamadı!");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        public int GetYazarId()
+        {
+            return int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+        }
+    }
+}
