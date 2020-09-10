4bbbc2e8b5031fb2fb1263deeb32912287208e39
R.Samet Özmen <sametozmen24@gmail.com>
2020-09-10 13:23:42 +0300

    CreateCategoryEntity

---
+++ ArticleApp.Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticleApp.Entities
+{
+    public class Category:BaseEntity
+    {
+        public string CategoryName { get; set; }
+
+    }
+}
